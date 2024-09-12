e9fac0c6b76f67e62ee2a2863f18f16338a890b7
kerenCruz <keren.cruz@mx.nestle.com>
2024-09-11 20:38:30 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,7 +27,7 @@ st.write('The name on your Smoothie will be:',name_on_order)
 #my_dataframe = session.table("smoothies.public.fruit_options").select (col('FRUIT_NAME'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
 
-my_dataframe=session.table("SMOOTHIES.PUBLIC.FRUIT_OPTIONS").select(col('FRUIT_NAME'),col('SERCH_ON'))
+my_dataframe=session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SERCH_ON'))
 st.dataframe(data=my_dataframe, use_container_width=true)
 st.stop()
 
