da449e67c49f3a482e8513b39c5f508b6337e66c
kerenCruz <keren.cruz@mx.nestle.com>
2024-09-17 17:25:49 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 # Import python packages
 import streamlit as st
+import pandas as pd_df
 import requests
 #from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
@@ -28,8 +29,13 @@ st.write('The name on your Smoothie will be:',name_on_order)
 #st.dataframe(data=my_dataframe, use_container_width=True)
 
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SERCH_ON'))
-st.dataframe(data=my_dataframe, use_container_width=True)
+#st.dataframe(data=my_dataframe, use_container_width=True)
 #st.stop()
+
+#Convert the snowpart Dataframe to a Pandas Dataframe so we can use the LOC function
+pd_df = my_dataframe.to_pandas()
+st.dataframe(pd_df)
+st.stop()
 
 ingredients_list = st.multiselect (
     'Choose up to 5 ingredients'
