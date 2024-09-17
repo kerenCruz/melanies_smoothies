72ba66524d30564f88009ec06675001426d0bc81
kerenCruz <keren.cruz@mx.nestle.com>
2024-09-17 16:27:15 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,7 +28,7 @@ st.write('The name on your Smoothie will be:',name_on_order)
 #st.dataframe(data=my_dataframe, use_container_width=True)
 
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SERCH_ON'))
-#st.dataframe(data=my_dataframe, use_container_width=True)
+st.dataframe(data=my_dataframe, use_container_width=True)
 #st.stop()
 
 ingredients_list = st.multiselect (
